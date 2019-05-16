c7cf762f7e7d9c558078ccb2c8ec1ad2be06a1c6
Kelvin Hadi Pratama <kelvinhadipratama@gmail.com>
2019-05-16 10:18:27 +0700

    Initial Commit

---
+++ DataFlowExercise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DataFlowExercise
+//
+//  Created by Kelvin Hadi Pratama on 16/05/19.
+//  Copyright © 2019 Kelvin Hadi Pratama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
